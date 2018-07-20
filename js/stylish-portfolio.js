cfd78ecfc0b82f897eb3ce9defef59e3a6491dc3
BrianMichaelTaylor <brianmtaylor22@gmail.com>
2018-07-20 11:11:41 -0500

    Udate background

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
+  $(".menu-toggle").click(function (e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
